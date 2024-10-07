e7676169e45c872cdfd90f7e0ab1c4c13eea90a8
Sinayel <yanis.louvel402@gmail.com>
2024-10-07 20:04:20 +0200

    echo add

--- includes/include.h
+++ includes/include.h
@@ -1,4 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   include.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ylouvel <ylouvel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/07 17:16:23 by ylouvel           #+#    #+#             */
+/*   Updated: 2024/10/07 19:55:14 by ylouvel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <readline/history.h>
+#include <readline/readline.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
-#include "../parsing/parsing_echo.c"
+
+// TODO -----------  Echo  -----------
+int		echo(char *str[]);
+void	print_string(char *str[]);
+void	print_string_with_option(char *str[]);
+int		echo_for_one_caractere(char *input);
+
+//? ------------  Utils  ------------
+int		ft_strcmp(const char *s1, const char *s2);
+char	**ft_split(char *str);
+int		word_count(char *str);
---
+++ srcs/parsing/parsing_echo.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing_echo.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ylouvel <ylouvel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/07 17:16:14 by ylouvel           #+#    #+#             */
+/*   Updated: 2024/10/07 20:02:49 by ylouvel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/include.h"
+
+void	print_string(char *str[])
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	j = 0;
+	while (str[i])
+	{
+		j = 0;
+		while (str[i][j])
+		{
+			write(1, &str[i][j], 1);
+			j++;
+		}
+		if (str[i][j] == '\0' && str[i + 1] != NULL)
+		{
+			write(1, " ", 1);
+		}
+		i++;
+	}
+}
+
+void	print_string_with_option(char *str[])
+{
+	int	i;
+	int	j;
+
+	i = 2;
+	j = 0;
+	while (str[i])
+	{
+		j = 0;
+		while (str[i][j])
+		{
+			write(1, &str[i][j], 1);
+			j++;
+		}
+		if (str[i][j] == '\0' && str[i + 1] != NULL)
+		{
+			write(1, " ", 1);
+		}
+		i++;
+	}
+}
+
+int	echo_for_one_caractere(char *input)
+{
+	printf("dzdqzd");
+	while (input[i])
+	{
+	}
+	
+	if (ft_strcmp(input, "echo") == 0)
+		printf("\n");
+	return (0);
+}
+
+int	echo(char *str[])
+{
+	if (ft_strcmp(str[1], "-n") == 0 && str != NULL)
+		print_string_with_option(str);
+	else if (ft_strcmp(str[1], "-n") != 0 && str != NULL)
+	{
+		print_string(str);
+		printf("\n");
+	}
+	return (0);
+}
