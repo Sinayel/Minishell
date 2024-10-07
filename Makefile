c533cc074c21d0a28b3c61474db3768ceab4b965
Sinayel <yanis.louvel402@gmail.com>
2024-10-07 20:04:20 +0200

    echo add

---
+++ Makefile
@@ -0,0 +1,35 @@
+CC = 			cc
+CFLAGS = 		-Wall -Wextra -Werror -lreadline -g
+
+RM = 			rm -f
+
+NAME = 			Minishell
+
+MINISHELL_SRC = 	main.c \
+					srcs/parsing/parsing_echo.c \
+					srcs/utils/utils.c \
+
+
+all :		$(NAME)
+
+$(NAME) :
+				@echo 'Creating Minishell... 🕗\n'
+				@${CC} ${MINISHELL_SRC} ${CFLAGS} -o ${NAME}
+				@echo 'Minishell created ! 🎉'
+
+re : 			
+				@$(RM) $(NAME)
+				@echo 'Minishell deleted ! 🛑\n'
+				@echo 'Creating Minishell... 🕗\n'
+				@${CC} ${MINISHELL_SRC} ${CFLAGS} -o ${NAME}
+				@echo 'Minishell created ! 🎉'
+
+clean:
+				@$(RM) $(NAME)
+				@echo 'Minishell deleted ! 🛑'
+
+fclean:
+				$(RM) $(NAME)
+				@echo 'Minishell deleted ! 🛑'
+
+.PHONY: 	fclean clean re all
---
+++ srcs/utils/utils.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ylouvel <ylouvel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/07 17:19:04 by ylouvel           #+#    #+#             */
+/*   Updated: 2024/10/07 20:01:08 by ylouvel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/include.h"
+
+char	*ft_strncpy(char *s1, char *s2, int n)
+{
+	int	i;
+
+	i = -1;
+	while (++i < n && s2[i])
+		s1[i] = s2[i];
+	s1[i] = '\0';
+	return (s1);
+}
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	size_t	i;
+
+	i = 0;
+	while (s1[i] || s2[i])
+	{
+		if ((unsigned char)s1[i] != (unsigned char)s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
+	}
+	return (0);
+}
+
+int	word_count(char *str)
+{
+	int	i;
+	int	wc;
+
+	i = 0;
+	wc = 0;
+	while (str[i])
+	{
+		while (str[i] && (str[i] == ' ' || str[i] == '\n' || str[i] == '\t'))
+			i++;
+		if (str && str[i] != '\0')
+			wc++;
+		while (str[i] && (str[i] != ' ' && str[i] != '\n' && str[i] != '\t'))
+			i++;
+	}
+	return (wc);
+}
+
+char	**ft_split(char *str)
+{
+	int		i;
+	int		j;
+	int		k;
+	char	**out;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	out = (char **)malloc(sizeof(char *) * (word_count(str) + 1));
+	while (str[i])
+	{
+		while (str[i] && (str[i] == ' ' || str[i] == '\n' || str[i] == '\t'))
+			i++;
+		j = i;
+		while (str[i] && (str[i] != ' ' && str[i] != '\n' && str[i] != '\t'))
+			i++;
+		if (i > j)
+		{
+			out[k] = (char *)malloc(sizeof(char *) * ((i - j) + 1));
+			ft_strncpy(out[k++], &str[j], i - j);
+		}
+	}
+	out[k] = NULL;
+	return (out);
+}
